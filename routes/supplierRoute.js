69ad093c9fb67d568a31956378594a6e670092ef
Ramath <ramathmanjitha@gmail.com>
2022-10-27 23:43:24 +0530

    supplier routes

---
+++ routes/supplierRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  addSupplier,
+  getOneSupplier,
+  getSuppliers,
+  deleteSupplier,
+  updateSupplierById,
+} from "../controllers/supplierController";
+
+var supplierRouter = express.Router();
+
+supplierRouter.post("/add-supplier", addSupplier);
+supplierRouter.get("/", getSuppliers);
+supplierRouter.get("/:supplierId", getOneSupplier);
+supplierRouter.delete("/:supplierId", deleteSupplier);
+supplierRouter.put("update/:supplierId", updateSupplierById);
+
+export default supplierRouter;
