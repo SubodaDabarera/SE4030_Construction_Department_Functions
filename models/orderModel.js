b46862d332712910cf387eab861750e6989be4a1
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-11-01 20:45:18 +0530

    order status updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,29 +1,35 @@
 import mongoose from "mongoose";
 
-const orderSchema = mongoose.Schema({
-  ownerId: {
-    type: String,
-    required: [true, "please enter owner id"],
+const orderSchema = mongoose.Schema(
+  {
+    ownerId: {
+      type: String,
+      required: [true, "please enter owner id"],
+    },
+    siteManager: {
+      type: String,
+      required: [true, "please enter site manager's id"],
+    },
+    productId: {
+      type: String,
+      required: [true, "please enter Product id"],
+    },
+    unitPrice: {
+      type: Number,
+      required: [true, "please enter unit price"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "please enter quantity"],
+    },
+    status: {
+      type: String,
+      default: "pending"
+    },
   },
-  siteManager: {
-    type: String,
-    required: [true, "please enter site manager's id"],
-  },
-  productId: {
-    type: String,
-    required: [true, "please enter Product id"],
-  },
-  unitPrice: {
-    type: Number,
-    required: [true, "please enter unit price"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "please enter quantity"],
-  },
-  
-}, {
-  timestamp: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Order", orderSchema);
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllOrders, getOrderById, getOrderBySM, placeOrder } from "../controllers/orderController.js";
+import { getAllOrders, getOrderById, getOrderBySM, placeOrder, updateOrderById } from "../controllers/orderController.js";
 
 var orderRouter = express.Router();
 orderRouter.post("/place-order", placeOrder);
 orderRouter.get("/", getAllOrders);
 orderRouter.get("/sm-order-details", getOrderBySM);
 orderRouter.get("/order-details", getOrderById);
+orderRouter.put("/update-order-status", updateOrderById);
+
 
 export default orderRouter;
