265ea4bbc7f9ad3c8f57f47a0ac757bb41b910c6
KenuriWickramasinghe <vidsala99@gmail.com>
2023-10-04 00:45:00 +0530

    log messages and proper returns added

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,9 @@
 import productModel from "../models/productModel.js";
+import logger from "../services/logger.js";
 
 export const addProduct = async (req, res) => {
   const { owner, title, unitPrice, quantity, location, description, imgUrl } =
     req.body;
-
   try {
     const newProduct = await productModel.create({
       owner,
@@ -14,60 +14,72 @@ export const addProduct = async (req, res) => {
       description,
       imgUrl,
     });
+    logger.info("New Product Added");
     return res.status(201).json({ success: true, Product: newProduct });
   } catch (err) {
-    console.log(err);
-    res.json(err);
+    logger.error("Product add is not successfull : ", err);
+    res.status(400).json({ success: false, Product: [] });
   }
 };
 
 export const getProducts = async (req, res) => {
-
   productModel
     .find()
     .then((products) => {
+      logger.info("All product details were retrieved");
+      if (products.length === 0) {
+        logger.info(
+          "No products were found, Num of products : ",
+          products.length
+        );
+      }
       res.json({ success: true, existingProducts: products });
     })
     .catch((err) => {
-      res.json(err);
-      console.log(err);
+      logger.error("Product retrieve is not successfull : ", err);
+      res.status(400).json({ success: false, existingProducts: [] });
     });
 };
 
 export const getOneProduct = async (req, res) => {
   const productId = req.params.productId;
+  logger.info(productId ? "Product Id is found" : "Product Id is not found");
 
   productModel
     .findById({ _id: productId })
     .then((product) => {
+      logger.info("Product retrieved");
+      if (!product) {
+        logger.info("No product in the database");
+      }
       res.json({ success: true, existingProduct: product });
     })
     .catch((err) => {
-      console.log(err);
-      res.json(err);
+      logger.error("Product retrieve is not successfull : ", err);
+      res.status(400).json({ success: false, existingProducts: [] });
     });
 };
 
 export const deleteProduct = async (req, res) => {
   const productId = req.params.productId;
+  logger.info(productId ? "Product Id is found" : "Product Id is not found");
 
   productModel
     .findByIdAndDelete(productId)
     .then(() => {
-      res.status(200).send({ status: "Item deleted " });
+      logger.info("Product deleted");
+      res.status(200).send({ success: true, status: "Item deleted" });
     })
     .catch((err) => {
-      console.log(err);
-      res.json(err);
+      logger.error("Product deletion is not successfull : ", err);
+      res.status(400).json({ success: false, status: "Item is not deleted" });
     });
 };
 
 export const updateProductById = async (req, res) => {
   const productId = req.params.productId;
-
-  console.log(productId);
-
   const { owner, title, unitPrice, quantity, location, description } = req.body;
+  logger.info(productId ? "Product Id is found" : "Product Id is not found");
 
   const updatedProduct = {
     owner,
@@ -81,10 +93,11 @@ export const updateProductById = async (req, res) => {
   productModel
     .findByIdAndUpdate(productId, updatedProduct)
     .then(() => {
+      logger.info("Product Updated");
       res.status(200).send({ status: "product updated" });
     })
     .catch((err) => {
-      console.log(err);
-      res.json(err);
+      logger.error("Product update is not successfull : ", err);
+      res.status(400).json({ success: false, status: "Item is not updated" });
     });
 };
